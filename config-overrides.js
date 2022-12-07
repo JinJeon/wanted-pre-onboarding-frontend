78c49092f86d92ceaa53ad5764b233e57e4cb911
JinJeon <nanyong01@gmail.com>
2022-12-07 23:24:38 +0900

    refactor : config overrides 리펙토링

--- config-overrides.js
+++ config-overrides.js
@@ -2,17 +2,24 @@ const path = require("path");
 
 const { override, addWebpackAlias } = require("customize-cra");
 
-module.exports = override(
-  addWebpackAlias({
-    "@api": path.resolve(__dirname, "src/api"),
-    "@constants": path.resolve(__dirname, "src/constants"),
-    "@components": path.resolve(__dirname, "src/components"),
-    "@hooks": path.resolve(__dirname, "src/hooks"),
-    "@pages": path.resolve(__dirname, "src/pages"),
-    "@router": path.resolve(__dirname, "src/router"),
-    "@store": path.resolve(__dirname, "src/store"),
-    "@styles": path.resolve(__dirname, "src/styles"),
-    "@type": path.resolve(__dirname, "src/type"),
-    "@utils": path.resolve(__dirname, "src/utils"),
-  }),
-);
+const alias = {};
+const directories = [
+  "api",
+  "constants",
+  "components",
+  "hooks",
+  "pages",
+  "router",
+  "store",
+  "styles",
+  "type",
+  "utils",
+];
+
+directories.forEach((directory) => {
+  const key = `@${directory}`;
+  const value = path.resolve(__dirname, `src/${directory}`);
+  alias[key] = value;
+});
+
+module.exports = override(addWebpackAlias(alias));
